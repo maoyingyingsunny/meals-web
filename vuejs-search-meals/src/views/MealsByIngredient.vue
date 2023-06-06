e850bfccd3c83640c94a5be0cfa5724991d68c3c
mao-yingying <2200022760@stu.pku.edu.cn>
2023-06-06 18:03:08 +0800

    更新 MealsByIngredient

--- vuejs-search-meals/src/views/MealsByIngredient.vue
+++ vuejs-search-meals/src/views/MealsByIngredient.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-    <div v-for="ingredient of ingredients" :key="ingredient.id"
-         class="bg-white rounded p-3 mb-3 shadow-xl">
+    <!-- 展示食物原料的卡片 -->
+    <router-link
+      :to="{
+      name:'byIngredient',
+      params:{ingredient:ingredient.strIngredient}
+      }"
+      v-for="ingredient of ingredients" :key="ingredient.id"
+      class="block bg-white rounded p-3 mb-3 shadow-xl">
+      <!-- 卡片标题及描述 -->
       <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
       <p>{{ ingredient.strDescription }}</p>
-    </div>
+    </router-link>
   </div>
 </template>
 
